88a6cda6b033766f408c050983fa6ab14834a294
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-07-15 20:28:07 +0430

    remove unnecessary comments

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -18,19 +18,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-// import { Fragment } from 'react';
-
-// import classes from './Layout.module.css';
-// import MainNavigation from './MainNavigation';
-
-// const Layout = (props) => {
-//   return (
-//     <Fragment>
-//       <MainNavigation />
-//       <main className={classes.main}>{props.children}</main>
-//     </Fragment>
-//   );
-// };
-
-// export default Layout;
