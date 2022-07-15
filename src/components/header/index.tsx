56a99f54e00d35f9b452099e6847d24bbd12c27b
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-07-15 20:15:10 +0430

    add logout functionality to lougout button on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { Row } from 'antd';
 import { Header as StyledHeader } from '@components/Header';
 import Logo from '../../shared/Logo/Logo';
@@ -9,13 +10,20 @@ interface Props {
   name: string
 }
 
-const Header: React.FC<Props> = ({ name }) => (
-  <StyledHeader>
-    <Row justify="space-between" align="middle">
-      <Logo color={colors.WHITE} />
-      <HeaderAccount name={name} onLogout={() => { console.log('logout'); }} />
-    </Row>
-  </StyledHeader>
-);
-
+const Header: React.FC<Props> = ({ name }) => {
+  const navigate = useNavigate();
+  return (
+    <StyledHeader>
+      <Row justify="space-between" align="middle">
+        <Logo color={colors.WHITE} />
+        <HeaderAccount
+          name={name}
+          onLogout={() => {
+            navigate('login', { replace: true });
+          }}
+        />
+      </Row>
+    </StyledHeader>
+  );
+};
 export default Header;
