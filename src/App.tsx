caa138c594890821bc020763ee2b186c74ab08b5
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-07-15 20:26:22 +0430

    remove unnecessary statement

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,12 @@ import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
-const App = () => {
-  const date = new Date();
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-  );
-};
+const App = () => (
+  <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="*" element={<Navigate to="/login" />} />
+  </Routes>
+);
 
 export default App;
