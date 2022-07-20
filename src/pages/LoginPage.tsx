110bd31600e5e50140de04d31dd35c53c72e6bd5
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-07-20 18:42:06 +0430

    add Card Components

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Typography, Row, Col, Image,
+  Typography, Row, Col, Image, Card,
 } from 'antd';
 import LoginForm from '@components/loginForm';
 import Logo from '@components/Logo';
@@ -25,11 +25,32 @@ const LoginPage: React.FC = () => {
   return (
     <Row justify='center' align='middle'>
       <Col xs={0} lg={12}>
+        <Card style={{
+          backgroundColor: 'green',
+          borderRadius: '40px',
+          position: 'absolute',
+          transform: 'rotate(30deg) translate(-200px, 150px)',
+          width: '700px',
+          height: '700px',
+          // zIndex: 10,
+        }}
+        />
+        <Card style={{
+          backgroundColor: 'red',
+          borderRadius: '40px',
+          transform: 'rotate(30deg) translate(-350px, 70px)',
+          position: 'absolute',
+          width: '700px',
+          height: '700px',
+          // zIndex: 1,
+
+        }}
+        />
         <Image
           preview={false}
           src={PetroImage}
           alt='PetroImage'
-          width='100%'
+          width='800px'
           height='100vh'
         />
       </Col>
